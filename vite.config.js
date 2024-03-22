be26aef7d9ee5a18014278911fd7acec02c3e6a6
Ming Wu <mingwu1214@gmail.com>
2024-03-22 23:05:36 +0800

    change base URL

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/live-vue3-training-chapter-2023/week6/dist/',
+  base: '/vue3-week6/dist/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
